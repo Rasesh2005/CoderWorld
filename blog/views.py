cc41b632110bc27d21adc1294cca60df5c701ebd
Rasesh2005 <rasesh.udayhsetty@gmail.com>
2020-10-10 00:23:37 +0530

    git commit -am "added Backend Logic For Reply"

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,8 @@ def blogHome(request):
 
 def blogPost(request,slug):
     post=Post.objects.filter(slug=slug).first()
-    comments=BlogComment.objects.filter(post=post)
+    comments=reversed(BlogComment.objects.filter(post=post,parent=None))
+    replies=reversed(BlogComment.objects.filter(post=post).exclude(parent=None))
     context={
         "post": post,
         'comments':comments
@@ -25,11 +26,17 @@ def postComment(request):
     if request.method=="POST":
         comment=request.POST['comment']
         user=request.user
-        sno=request.POST['sno']
+        sno=request.POST['postSno']
         post=Post.objects.get(sno=sno)
-        comment=BlogComment(comment=comment,user=user,post=post)
-        comment.save()
-        messages.success(request,'Your Comment Has been posted successfully')
-    else:
-        messages.error(request,'Some Error Occured While Commenting')
-    return redirect(f'/blog/{post.slug}/')
+        parentSno=request.POST['parentSno']
+        if parentSno=="":
+            comment=BlogComment(comment=comment,user=user,post=post)
+            comment.save()
+            messages.success(request,'Your Comment Has been posted successfully')
+        else:
+            parent=BlogComment.objects.get(sno=parentSno)
+            comment=BlogComment(comment=comment,user=user,post=post,parent=parent)
+            comment.save()
+            messages.success(request,'Your Reply Has been posted successfully')
+        return redirect(f'/blog/{post.slug}/')
+    return HttpResponse("Error 404 - Something Went Wrong")
